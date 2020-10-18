c93cafc51c6684e4548d5e8ee362c3d0868d2699
Hanna Mulugeta <hannabmulugeta@gmail.com>
2020-10-18 20:56:18 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -16,6 +16,5 @@ end
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
-  my_hash = Hash.new
-  my_hash[:id] = number
+  my_hash = {:id => number}
 end
