40978eae461dd839e5d640818d942fb87e800b31
Hanna Mulugeta <hannabmulugeta@gmail.com>
2020-10-18 20:08:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,10 @@
 def new_hash
-  new_hash = Hash.new# return an empty hash
+  new_hash = Hash.new # return an empty hash
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  my_hash = {:key => "value"}
 end
 
 def pioneer
